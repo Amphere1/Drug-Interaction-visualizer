a32559dee5bd3e61a8002cd2e978687b00a0ff61
Krishnakant Sahu <krishnakantsahu916@gmail.com>
2025-05-27 18:52:57 +0530

    fixed bugs

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,6 +1,6 @@
 import express from "express";
 import User from "../models/userModel.js";
-import bcrypt from "bcryptjs";
+import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import dotenv from 'dotenv';
 
